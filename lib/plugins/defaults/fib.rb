efbfd4e55059110938bc07bf432d44437da65d0f
hitode909 <hitode909@gmail.com>
2009-10-07 21:22:03 +0900

    fibyouをdefaults/fibに移動

--- lib/plugins/defaults/fib.rb
+++ lib/plugins/defaults/fib.rb
@@ -1,7 +1,19 @@
 def fib(n)i=0;j=1;n.times{j=i+i=j};i end
-Termtter::Client.register_command(:fib) do |arg|
-  n = arg.to_i
-  text = "fib(#{n}) = #{fib n}"
-  Termtter::API.twitter.update(text)
-  puts "=> " << text
+
+module Termtter::Client
+  register_command(:fib) do |arg|
+    n = arg.to_i
+    text = "fib(#{n}) = #{fib n}"
+    Termtter::API.twitter.update(text)
+    puts "=> " << text
+  end
+
+  register_command(:fibyou) do |arg|
+    /(\w+)\s(\d+)/ =~ arg
+    name = normalize_as_user_name($1)
+    n = $2.to_i
+    text = "@#{name} fib(#{n}) = #{fib n}"
+    Termtter::API.twitter.update(text)
+    puts "=> " << text
+  end
 end
