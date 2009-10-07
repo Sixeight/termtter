abcb414606698a23612be83693bf50e5f34c3e64
hitode909 <hitode909@gmail.com>
2009-10-07 20:38:43 +0900

    coolとhiをgreetingにまとめた

--- lib/plugins/greeting.rb
+++ lib/plugins/greeting.rb
@@ -1,7 +1,12 @@
 # -*- coding: utf-8 -*-
-
+# TODO: make register_greeting command
 module Termtter::Client
   register_macro(:cool, "update %s cool.",
     :help => ['cool {SCREENNAME}', 'update "@{SCREENNAME} cool."']
   )
+
+  register_macro(:hi, "update %s hi",
+    :help => ['hi {SCREENNAME}', 'update "@{SCREENNAME} hi"']
+  )
+
 end
