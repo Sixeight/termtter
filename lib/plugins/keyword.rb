56f1a824f6cc76ef1fdbbbee75d81ce73d62aee8
hitode909 <hitode909@gmail.com>
2009-10-07 20:05:47 +0900

    keywordとignoreを統合(テストなし)

--- lib/plugins/keyword.rb
+++ lib/plugins/keyword.rb
@@ -1,18 +1,31 @@
 # -*- coding: utf-8 -*-
-
-config.set_default('plugins.keyword.keywords', [])
-
-module Termtter
-  class Status
-    def has_keyword?
-      config.plugins.keyword.keywords.find { |k| k === self.text }
-    end
-    alias :has_keyword :has_keyword?
+config.plugins.keyword.set_default(:keywords, [])
+config.plugins.keyword.set_default(:ignores, [])
+module Termtter::Client
+  register_hook(
+    :name => :highlight_keywords,
+    :point => :pre_coloring) do |text, event|
+    query = config.plugins.keyword.keywords.map {|q|Regexp.quote(q)}.join("|")
+    text.gsub(/(#{query})/i, '<on_green><white>\1</white></on_green>')
   end
+
+  register_hook(
+    :name => :ignore,
+    :point => :filter_for_output,
+    :exec => lambda { |statuses, event|
+      ignore_words = config.plugins.keyword.ignores
+      statuses.delete_if do |s|
+        ignore_words.any? do |word|
+          word = /#{Regexp.quote(word)}/ if word.kind_of? String
+          word =~ s.text
+        end
+      end
+    }
+  )
 end
 
 # keyword.rb
-#   provides a keyword watching method
-# example config
-#   config.plugins.stdout.timeline_format = '<%= color(time, 90) %> <%= color(status, s.has_keyword ? 4 : status_color) %> <%= color(id, 90) %>'
-#   config.plugins.keyword.keywords = [ /motemen/ ]
+#   provides a keyword watching method and ignore methods
+# setting
+#   config.plugins.keyword.keywords = [ 'key', 'words' ]
+#   config.plugins.keyword.ignores  = [ 'key', 'words' ]
