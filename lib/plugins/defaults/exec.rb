023ec3c10b88afd6b1b1ddfaa55b8b5c22488c58
hitode909 <hitode909@gmail.com>
2009-10-07 20:09:50 +0900

    exec_and_updateをexecに統合

--- lib/plugins/defaults/exec.rb
+++ lib/plugins/defaults/exec.rb
@@ -9,4 +9,19 @@ module Termtter::Client
     },
     :help => ['exec SHELL_COMMAND', 'execute a shell command']
   )
+
+  register_command(
+    :name => :exec_and_update,
+    :exec_proc => lambda{|arg|
+      return unless arg
+      `#{arg}`.each_line do |line|
+        next if line =~ /^\s*$/
+        Termtter::API.twitter.update(line)
+        puts "=> #{line}"
+      end
+    },
+    :help => ['exec_and_update COMMAND', 'execute and update the command']
+  )
+
 end
+
